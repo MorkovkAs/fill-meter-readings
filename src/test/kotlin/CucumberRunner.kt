5b9574d0f9a04b9dd65609e6190d37be4abc37bf
MorkovkAs <avklimakov@gmail.com>
2020-08-19 16:07:20 +0300

    fix of cucumber test

--- src/test/kotlin/CucumberRunner.kt
+++ src/test/kotlin/CucumberRunner.kt
@@ -2,7 +2,7 @@ import io.cucumber.junit.Cucumber
 import io.cucumber.junit.CucumberOptions
 import org.junit.runner.RunWith
 
-@RunWith(Cucumber::class)
+//@RunWith(Cucumber::class)
 @CucumberOptions(
     features = ["src/test/resources/cucumber/features"]
 )
